4a3612b0e0c1a40a746ffc5a8172b66b211a53f3
gisaki <gisaki@gmail.com>
2018-02-27 01:05:46 +0900

    Commit prototype version

---
+++ Mqtt2Udp/Mqtt2Udp/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Mqtt2Udp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_MQTT_Con = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button_MQTT_Pub = new System.Windows.Forms.Button();
+            this.textBoxHost = new System.Windows.Forms.TextBox();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxTopicSub = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxTopicPub = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox_UDPIP = new System.Windows.Forms.TextBox();
+            this.textBox_UDPPort = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_MQTT_Con
+            // 
+            this.button_MQTT_Con.Location = new System.Drawing.Point(12, 81);
+            this.button_MQTT_Con.Name = "button_MQTT_Con";
+            this.button_MQTT_Con.Size = new System.Drawing.Size(84, 23);
+            this.button_MQTT_Con.TabIndex = 0;
+            this.button_MQTT_Con.Text = "Connect";
+            this.button_MQTT_Con.UseVisualStyleBackColor = true;
+            this.button_MQTT_Con.Click += new System.EventHandler(this.button_MQTT_Con_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 203);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(435, 119);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button_MQTT_Pub
+            // 
+            this.button_MQTT_Pub.Location = new System.Drawing.Point(12, 110);
+            this.button_MQTT_Pub.Name = "button_MQTT_Pub";
+            this.button_MQTT_Pub.Size = new System.Drawing.Size(84, 23);
+            this.button_MQTT_Pub.TabIndex = 2;
+            this.button_MQTT_Pub.Text = "Publish";
+            this.button_MQTT_Pub.UseVisualStyleBackColor = true;
+            this.button_MQTT_Pub.Click += new System.EventHandler(this.button_MQTT_Pub_Click);
+            // 
+            // textBoxHost
+            // 
+            this.textBoxHost.Location = new System.Drawing.Point(179, 14);
+            this.textBoxHost.Name = "textBoxHost";
+            this.textBoxHost.Size = new System.Drawing.Size(268, 19);
+            this.textBoxHost.TabIndex = 3;
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(179, 39);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(268, 19);
+            this.textBoxUsername.TabIndex = 3;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(179, 64);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(268, 19);
+            this.textBoxPassword.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Host";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(109, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password";
+            // 
+            // textBoxTopicSub
+            // 
+            this.textBoxTopicSub.Location = new System.Drawing.Point(179, 89);
+            this.textBoxTopicSub.Name = "textBoxTopicSub";
+            this.textBoxTopicSub.Size = new System.Drawing.Size(268, 19);
+            this.textBoxTopicSub.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(109, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Topic(sub)";
+            // 
+            // textBoxTopicPub
+            // 
+            this.textBoxTopicPub.Location = new System.Drawing.Point(179, 114);
+            this.textBoxTopicPub.Name = "textBoxTopicPub";
+            this.textBoxTopicPub.Size = new System.Drawing.Size(268, 19);
+            this.textBoxTopicPub.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(109, 117);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 12);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Topic(pub)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(109, 167);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 12);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "UDP";
+            // 
+            // textBox_UDPIP
+            // 
+            this.textBox_UDPIP.Location = new System.Drawing.Point(179, 164);
+            this.textBox_UDPIP.Name = "textBox_UDPIP";
+            this.textBox_UDPIP.Size = new System.Drawing.Size(182, 19);
+            this.textBox_UDPIP.TabIndex = 8;
+            this.textBox_UDPIP.Text = "255.255.255.255";
+            // 
+            // textBox_UDPPort
+            // 
+            this.textBox_UDPPort.Location = new System.Drawing.Point(367, 164);
+            this.textBox_UDPPort.Name = "textBox_UDPPort";
+            this.textBox_UDPPort.Size = new System.Drawing.Size(80, 19);
+            this.textBox_UDPPort.TabIndex = 9;
+            this.textBox_UDPPort.Text = "1883";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 334);
+            this.Controls.Add(this.textBox_UDPPort);
+            this.Controls.Add(this.textBox_UDPIP);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxTopicPub);
+            this.Controls.Add(this.textBoxTopicSub);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.textBoxHost);
+            this.Controls.Add(this.button_MQTT_Pub);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_MQTT_Con);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_MQTT_Con;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button_MQTT_Pub;
+        private System.Windows.Forms.TextBox textBoxHost;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxTopicSub;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxTopicPub;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox_UDPIP;
+        private System.Windows.Forms.TextBox textBox_UDPPort;
+    }
+}
+
---
+++ Mqtt2Udp/Mqtt2Udp/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+
+namespace Mqtt2Udp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBoxHost.Text = Properties.Settings.Default.host;
+            textBoxUsername.Text = Properties.Settings.Default.username;
+            textBoxPassword.Text = Properties.Settings.Default.password;
+            textBoxTopicSub.Text= Properties.Settings.Default.topic_sub;
+            textBoxTopicPub.Text = Properties.Settings.Default.topic_pub;
+            textBox_UDPIP.Text = Properties.Settings.Default.udp_ip;
+            textBox_UDPPort.Text = Properties.Settings.Default.udp_port;
+        }
+
+        // サーバ証明書の検証
+        private static Boolean RemoteCertificateValidationCallback(Object sender,
+            X509Certificate certificate,
+            X509Chain chain,
+            SslPolicyErrors sslPolicyErrors)
+        {
+            //　サーバ証明書を検証せず、常に許容
+            return true;
+        }
+        
+        private MqttClient client_ = null;
+        private void button_MQTT_Con_Click(object sender, EventArgs e)
+        {
+            string host = textBoxHost.Text;
+            string username = textBoxUsername.Text;
+            string password = textBoxPassword.Text;
+            string topicsub = textBoxTopicSub.Text;
+
+            // create client instance
+            this.client_ = new MqttClient(host, 8883, true, MqttSslProtocols.TLSv1_2, RemoteCertificateValidationCallback, null);
+
+            // register to message received
+            this.client_.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            string clientId = Guid.NewGuid().ToString();
+            this.client_.Connect(clientId, username, password);
+
+            // subscribe to the topic "/home/temperature" with QoS 0
+            this.client_.Subscribe(
+                new string[] { topicsub }, 
+                new byte[] { uPLibrary.Networking.M2Mqtt.Messages.MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE }
+            );
+
+        }
+
+        private void client_MqttMsgPublishReceived(object sender, uPLibrary.Networking.M2Mqtt.Messages.MqttMsgPublishEventArgs e)
+        {
+            // handle message received
+            this.textBox1.Invoke((Action)(() =>
+            {
+                textBox1.Text += string.Format("[{0}][{1}]", DateTime.Now, e.Topic);
+                string msg = Encoding.UTF8.GetString(e.Message);
+                textBox1.Text += msg;
+                textBox1.Text += "\r\n";
+
+                // raw
+                textBox1.Text += string.Format("[{0}]\r\n", BitConverter.ToString(e.rcvbuf));
+
+                {
+                    // 送受信に利用するIPアドレス、ポート番号
+                    int port = Int32.Parse(textBox_UDPPort.Text);
+                    System.Net.IPAddress ipaddress = System.Net.IPAddress.Parse(textBox_UDPIP.Text);
+                    //System.Net.IPAddress ipaddress = System.Net.IPAddress.Broadcast;
+
+                    // 送信データ
+                    byte[] buffer = e.rcvbuf;
+
+                    // UDP送信
+                    var client = new System.Net.Sockets.UdpClient(port);
+                    client.EnableBroadcast = true;
+                    client.Connect(new System.Net.IPEndPoint(ipaddress, port));
+                    client.Send(buffer, buffer.Length);
+                    client.Close();
+                }
+
+            }));
+        }
+
+        private void button_MQTT_Pub_Click(object sender, EventArgs e)
+        {
+            if (this.client_ == null) 
+            {
+                return;
+            }
+            string topicpub = textBoxTopicPub.Text;
+            this.client_.Publish(
+                topicpub, 
+                Encoding.UTF8.GetBytes("hello vs!"), 
+                uPLibrary.Networking.M2Mqtt.Messages.MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, 
+                false
+            );
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Properties.Settings.Default.host = textBoxHost.Text;
+            Properties.Settings.Default.username = textBoxUsername.Text;
+            Properties.Settings.Default.password = textBoxPassword.Text;
+            Properties.Settings.Default.topic_sub = textBoxTopicSub.Text;
+            Properties.Settings.Default.topic_pub = textBoxTopicPub.Text;
+            Properties.Settings.Default.udp_ip = textBox_UDPIP.Text;
+            Properties.Settings.Default.udp_port = textBox_UDPPort.Text;
+            Properties.Settings.Default.Save();
+
+            if (this.client_ == null)
+            {
+                return;
+            }
+            this.client_.Disconnect();
+        }
+    }
+}
